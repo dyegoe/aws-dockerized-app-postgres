11f45e00c4782a719d1c74170e107db39a75ea03
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-10-14 16:22:01 +0300

    [update] couple of parameters

--- terraform/11_rds.tf
+++ terraform/11_rds.tf
@@ -10,12 +10,14 @@ resource "aws_db_subnet_group" "main" {
 }
 
 resource "aws_rds_cluster_instance" "main" {
-  count = 2
+  count = "${var.db_instance_count}"
   identifier = "${var.project_name}-rds-${count.index}"
   cluster_identifier = "${aws_rds_cluster.main.id}"
+  engine = "aurora-postgresql"
   instance_class = "${var.db_instance}"
   publicly_accessible = false
   db_subnet_group_name = "${aws_db_subnet_group.main.id}"
+  apply_immediately = true
   tags = "${merge(
     local.common_tags,
     map(
@@ -26,14 +28,20 @@ resource "aws_rds_cluster_instance" "main" {
 
 resource "aws_rds_cluster" "main" {
   cluster_identifier = "${var.project_name}-rds"
-  engine = "aurora-postgresql"
-  availability_zones = ["${aws_subnet.public.*.availability_zone}"]
-  db_subnet_group_name = "${aws_db_subnet_group.main.id}"
   database_name = "${var.db_name}"
-  master_username = "${var.db_user}"
   master_password = "${var.db_password}"
+  master_username = "${var.db_user}"
+  skip_final_snapshot = false
+  availability_zones = ["${aws_subnet.public.*.availability_zone}"]
   backup_retention_period = 5
   preferred_backup_window = "07:00-09:00"
   vpc_security_group_ids = ["${aws_security_group.rds.id}"]
   apply_immediately = true
+  db_subnet_group_name = "${aws_db_subnet_group.main.id}"
+  tags = "${merge(
+    local.common_tags,
+    map(
+      "Name", "${var.project_name}-rds-cls"
+    )
+  )}"
 }
