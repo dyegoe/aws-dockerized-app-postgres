6996a065cd9fec2665dedebe4ae032562eaec8cc
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-10-14 20:42:53 +0300

    [update] include db create call

--- src/runserver.py
+++ src/runserver.py
@@ -1,7 +1,9 @@
 from notejam import app
 from notejam.config import ProductionConfig
+from notejam import db
 
 app.config.from_object(ProductionConfig)
 
 if __name__ == '__main__':
+    db.create_all()
     app.run(host='0.0.0.0')
