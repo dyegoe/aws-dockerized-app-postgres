36bcb9603120d51ce6bc954d87852a6e332c9ce9
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-10-14 17:43:23 +0300

    [fix] rules

--- terraform/06_sg.tf
+++ terraform/06_sg.tf
@@ -56,6 +56,12 @@ resource "aws_security_group" "lb-ec2" {
     protocol = "TCP"
     security_groups = ["${aws_security_group.lb.id}"]
   }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = "${merge(
     local.common_tags,
     map(
@@ -72,7 +78,13 @@ resource "aws_security_group" "rds" {
     from_port = 5432
     to_port = 5432
     protocol = "TCP"
-    security_groups = ["${aws_security_group.lb-ec2.id}"]
+    security_groups = ["${aws_security_group.ssh.id}"]
+  }
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
   tags = "${merge(
     local.common_tags,
