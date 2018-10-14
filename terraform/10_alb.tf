8226a01cce1f0a1ea3ea46724f354ee4c98a4f67
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-10-14 16:01:07 +0300

    [update] remove delete protection

--- terraform/10_alb.tf
+++ terraform/10_alb.tf
@@ -1,15 +1,12 @@
 resource "aws_lb" "main" {
-  name               = "${var.project_name}-lb"
-  internal           = false
+  name = "${var.project_name}-lb"
+  internal = false
   load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.lb.id}"]
-  subnets            = ["${aws_subnet.public-a.id}", "${aws_subnet.public-b.id}"]
-
-  enable_deletion_protection = true
-
+  security_groups = ["${aws_security_group.lb.id}"]
+  subnets = ["${aws_subnet.public.*.id}"]
   access_logs {
-    bucket  = "${aws_s3_bucket.lb-logs.id}"
-    prefix  = "${var.project_name}-lb"
+    bucket = "${aws_s3_bucket.lb-logs.id}"
+    prefix = "${var.project_name}-lb"
     enabled = true
   }
 
@@ -22,10 +19,10 @@ resource "aws_lb" "main" {
 }
 
 resource "aws_lb_target_group" "main" {
-  name     = "${var.project_name}-lb-tg-main"
-  port     = 80
+  name = "${var.project_name}-lb-tg-main"
+  port = 80
   protocol = "HTTP"
-  vpc_id   = "${aws_vpc.main.id}"
+  vpc_id = "${aws_vpc.main.id}"
   tags = "${merge(
     local.common_tags,
     map(
@@ -36,10 +33,10 @@ resource "aws_lb_target_group" "main" {
 
 resource "aws_lb_listener" "main" {
   load_balancer_arn = "${aws_lb.main.arn}"
-  port              = "80"
-  protocol          = "HTTP"
+  port = "80"
+  protocol = "HTTP"
   default_action {
-    type             = "forward"
+    type = "forward"
     target_group_arn = "${aws_lb_target_group.main.arn}"
   }
 }
