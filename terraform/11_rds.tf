4462c83a74a1650e5ff6a3c99a1e649def9bfe6f
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-10-14 17:08:36 +0300

    [update] move from rds module to db_instance

--- terraform/11_rds.tf
+++ terraform/11_rds.tf
@@ -9,40 +9,32 @@ resource "aws_db_subnet_group" "main" {
   )}"
 }
 
-resource "aws_rds_cluster_instance" "main" {
+resource "aws_db_instance" "default" {
   count = "${var.db_instance_count}"
-  identifier = "${var.project_name}-rds-${count.index}"
-  cluster_identifier = "${aws_rds_cluster.main.id}"
-  engine = "aurora-postgresql"
-  instance_class = "${var.db_instance}"
-  publicly_accessible = false
+  allocated_storage = 10
+  apply_immediately = false
+  availability_zones = ["${aws_subnet.public.*.availability_zone}"]
+  backup_retention_period = 5
+  backup_window = "05:00-07:00"
+  copy_tags_to_snapshot = true
   db_subnet_group_name = "${aws_db_subnet_group.main.id}"
-  apply_immediately = true
+  engine = "postgres"
+  engine_version = "10.4-R1"
+  identifier = "${var.project_name}-rds-${count.index}"
+  instance_class = "${var.db_instance}"
+  maintenance_window = "Mon:00:00-Mon:03:00"
+  name = "${var.db_name}"
+  parameter_group_name = "default.postgres10"
+  password = "${var.db_password}"
+  publicly_accessible = false
+  skip_final_snapshot = true
+  storage_type = "gp2"
   tags = "${merge(
     local.common_tags,
     map(
       "Name", "${var.project_name}-rds-${count.index}"
     )
   )}"
-}
-
-resource "aws_rds_cluster" "main" {
-  cluster_identifier = "${var.project_name}-rds"
-  database_name = "${var.db_name}"
-  master_password = "${var.db_password}"
-  master_username = "${var.db_user}"
-  skip_final_snapshot = true
-  availability_zones = ["${aws_subnet.public.*.availability_zone}"]
-  backup_retention_period = 5
-  preferred_backup_window = "07:00-09:00"
+  username = "${var.db_user}"
   vpc_security_group_ids = ["${aws_security_group.rds.id}"]
-  apply_immediately = true
-  db_subnet_group_name = "${aws_db_subnet_group.main.id}"
-  engine = "aurora-postgresql"
-  tags = "${merge(
-    local.common_tags,
-    map(
-      "Name", "${var.project_name}-rds-cls"
-    )
-  )}"
 }
