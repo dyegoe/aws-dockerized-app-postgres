42ab3daffaa557c6eca1705b9ec8916a7ef05b20
Dyego Alexandre Eugenio <dyegoe@gmail.com>
2018-10-19 15:14:56 +0300

    [create] iam user to attach to instances

--- terraform/04_user.tf
+++ terraform/04_user.tf
@@ -40,3 +40,39 @@ output "aws_iam_access_key_ecr_user_id" {
 output "aws_iam_access_key_ecr_user_secret" {
   value = "${aws_iam_access_key.ecr_user.secret}"
 }
+
+resource "aws_iam_user" "ec2_profile" {
+  name = "ec2_profile"
+  path = "/system/"
+}
+
+resource "aws_iam_access_key" "ec2_profile" {
+  user = "${aws_iam_user.ec2_profile.id}"
+}
+
+resource "aws_iam_user_policy" "ec2_profile" {
+  name = "ec2_cloudwatch_policy"
+  user = "${aws_iam_user.ec2_profile.id}"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": "logs:*",
+            "Resource": "*"
+        }
+    ]
+}
+EOF
+}
+
+output "aws_iam_access_key_ec2_cloudwatch_id" {
+  value = "${aws_iam_access_key.ec2_profile.id}"
+}
+
+output "aws_iam_access_key_ec2_cloudwatch_secret" {
+  value = "${aws_iam_access_key.ec2_profile.secret}"
+}
